e0ab0eb38688169ae240235775c394c75de6ea84
Gaby8973621 <gabriela472151@gmail.com>
2025-12-08 20:42:40 -0600

    lecciones
    agregue lecciones

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,12 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
 import Home from '../views/Home.vue'
+import Lecciones from '../views/Lecciones.vue'
+
 
 const routes = [
   { path: '/', redirect: '/login' }, 
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
-  { path: '/home', name: 'Home', component: Home }
+  { path: '/home', name: 'Home', component: Home },
+  { path: '/lecciones', name: 'Lecciones', component: Lecciones }
 ]
 
 const router = createRouter({
---
+++ frontend/src/views/Lecciones.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <!-- 🔙 Botón Volver -->
+    <button class="btn-back" @click="goBack">
+      <i class="bi bi-arrow-left"></i>
+    </button> 
+
+  <div class="container text-center py-5">
+    <h1 class="fw-bold" style="color:#a62681">📘 Lecciones</h1>
+    <p class="mt-3">Selecciona una lección para comenzar</p>
+
+    <div class="row g-2 justify-content-center">
+<div class="col-10 col-md-4 col-lg-3 mb-4" v-for="l in lessons" :key="l.id">        <div class="p-4 lesson-card">
+          <h4 class="fw-bold">{{ l.title }}</h4>
+          <button class="btn btn-primary mt-3">Entrar</button>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      lessons: [
+        { id: 1, title: "Vocales" },
+        { id: 2, title: "Consonantes" },
+        { id: 3, title: "Sílabas" },
+      ],
+    };
+  },
+
+  methods: {
+  goBack() {
+    this.$router.back()
+  }
+}
+
+};
+</script>
+
+<style src="../assets/styles/lecciones.css"></style>
+
